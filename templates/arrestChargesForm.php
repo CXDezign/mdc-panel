a9aa36828be8b7cca4a0fb05938314f3893dc85c
xanxTV <29733018+XanXTV@users.noreply.github.com>
2020-03-06 16:25:34 +0000

    Arrest Charges Form - Icon Spacing Fixes

--- templates/arrestChargesForm.php
+++ templates/arrestChargesForm.php
@@ -1,8 +1,7 @@
 <div class="container mb-5 pb-5">
 	<h1 class="my-3">Arrest Report - Charges</h1>
 	<form action="index.php?page=arrestReport" method="POST">
-
-		<h4><i class="fas fa-receipt fa-fw"></i> Charges</h4>
+		<h4><i class="fas fa-fw fa-receipt mr-2"></i>Charges</h4>
 		<div class="form-row chargeGroup">
 			<div class="form-group col-xl-6">
 				<label>Crime ID, Title, & Classification</label>
@@ -48,17 +47,17 @@
 			<div class="form-group col-xl-2">
 				<label>Options</label>
 				<div class="input-group-addon"> 
-					<a href="javascript:void(0)" class="btn btn-success w-100 addCharge"><i class="fas fa-plus-square fa-fw"></i> Add Charge</a>
+					<a href="javascript:void(0)" class="btn btn-success w-100 addCharge"><i class="fas fa-fw fa-plus-square mr-1"></i>Add Charge</a>
 				</div>
 			</div>
 		</div>
 		
 		<div class="container mt-2 text-center">
-			<a class="btn btn-info px-5" target="_blank" href="https://forum.gta.world/en/index.php?/topic/20053-san-andreas-penal-code/" role="button"><i class="fas fa-archive fa-fw"></i> Open Penal Code</a>
+			<a class="btn btn-info px-5" target="_blank" href="https://forum.gta.world/en/index.php?/topic/20053-san-andreas-penal-code/" role="button"><i class="fas fa-archive fa-fw mr-1"></i>Open Penal Code</a>
 		</div>
 		
 		<div class="container my-5 text-center">
-		<button id="submit" type="submit" name="submit" class="btn btn-primary px-5"><i class="fas fa-plus-square fa-fw"></i>Calculate Arrest</button>
+		<button id="submit" type="submit" name="submit" class="btn btn-primary px-5"><i class="fas fa-fw fa-plus-square mr-1"></i>Calculate Arrest</button>
 	</div>
 	</form>
 
@@ -103,7 +102,7 @@
 		</div>
 		<div class="form-group col-xl-2">
 			<div class="input-group-addon"> 
-				<button class="btn btn-danger w-100 removeCharge" type="button" id="button-addon2"><i class="fas fa-minus-square"></i> Remove Charge</button>
+				<button class="btn btn-danger w-100 removeCharge" type="button" id="button-addon2"><i class="fas fa-fw fa-minus-square mr-1"></i>Remove Charge</button>
 			</div>
 		</div>
 	</div>
