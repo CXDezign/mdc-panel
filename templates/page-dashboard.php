f9953ae2ce940156961a2f77cc17366b6f4db891
xanxTV <29733018+XanXTV@users.noreply.github.com>
2020-04-25 11:08:19 +0100

    Visual Update - Card Highlights

--- templates/page-dashboard.php
+++ templates/page-dashboard.php
@@ -5,7 +5,7 @@
 		<div class="col-xl-6 mb-4">
 			<a href="/street-guide">
 				<div class="card card-panel bg-dark text-white" id="card-main-map">
-					<div class="card-body">
+					<div class="card-body shadow">
 						<p class="card-text text-right float-right"><i class="fas fa-fw fa-7x fa-map-marker-alt text-muted"></i></p>
 						<h5 class="card-title">Street Guide</h5>
 						<p class="card-text">Find street locations, locate official government buildings, explore the various districts of the San Andreas state.</p>
@@ -16,7 +16,7 @@
 		<div class="col-xl-6 mb-4">
 			<a href="/paperwork-generators">
 				<div class="card card-panel bg-dark text-white" id="card-main-generators">
-					<div class="card-body">
+					<div class="card-body shadow">
 						<p class="card-text text-right float-right"><i class="fas fa-fw fa-7x fa-archive text-muted"></i></p>
 						<h5 class="card-title">Paperwork Generators</h5>
 						<p class="card-text">Generate a multitude different types of paperwork reports, logs, and more to assist your process.</p>
@@ -29,7 +29,7 @@
 		<div class="col-xl-6 mb-4">
 			<a href="/useful-resources">
 				<div class="card card-panel bg-dark text-white" id="card-main-resources">
-					<div class="card-body">
+					<div class="card-body shadow">
 						<p class="card-text text-right float-right"><i class="fas fa-fw fa-7x fa-book text-muted"></i></p>
 						<h5 class="card-title">Useful Resources</h5>
 						<p class="card-text">Access some quick and handy resources.</p>
@@ -40,7 +40,7 @@
 		<div class="col-xl-6 mb-4">
 			<a target="_blank" href="https://forum.gta.world/en/index.php?/topic/20053-san-andreas-penal-code/">
 				<div class="card card-panel bg-dark text-white" id="card-main-penal">
-					<div class="card-body">
+					<div class="card-body shadow">
 						<p class="card-text text-right float-right"><i class="fas fa-fw fa-7x fa-balance-scale text-muted"></i></p>
 						<h5 class="card-title">San Andreas Penal Code</h5>
 						<p class="card-text">Access the San Andreas Penal Code.</p>
--- templates/page-generators.php
+++ templates/page-generators.php
@@ -5,7 +5,7 @@
 		<div class="col-xl-6 mb-4">
 			<a href="/paperwork-generators/arrest-charges">
 				<div class="card card-panel bg-dark text-white" id="card-generators-arrest">
-					<div class="card-body">
+					<div class="card-body shadow">
 						<p class="card-text text-right float-right"><i class="fas fa-fw fa-7x fa-landmark text-muted"></i></p>
 						<h5 class="card-title">Arrest Report</h5>
 						<p class="card-text">Calculate time and fines from charges. Then generate an arrest report</p>
@@ -16,7 +16,7 @@
 		<div class="col-xl-6 mb-4">
 			<a href="/paperwork-generators/traffic-report">
 				<div class="card card-panel bg-dark text-white" id="card-generators-traffic">
-					<div class="card-body">
+					<div class="card-body shadow">
 						<p class="card-text text-right float-right"><i class="fas fa-fw fa-7x fa-car text-muted"></i></p>
 						<h5 class="card-title">Traffic Report</h5>
 						<p class="card-text">Generate a traffic report when conducting traffic stops.</p>
@@ -29,7 +29,7 @@
 		<div class="col-xl-6 mb-4">
 			<a href="/paperwork-generators/evidence-registration-log">
 				<div class="card card-panel bg-dark text-white" id="card-generators-evidence">
-					<div class="card-body">
+					<div class="card-body shadow">
 						<p class="card-text text-right float-right"><i class="fas fa-fw fa-7x fa-cannabis text-muted"></i></p>
 						<h5 class="card-title">Evidence Registration Log</h5>
 						<p class="card-text">Generate an evidence registration log.</p>
@@ -40,7 +40,7 @@
 		<div class="col-xl-6 mb-4">
 			<a href="/paperwork-generators/death-report">
 				<div class="card card-panel bg-dark text-white" id="card-generators-death">
-					<div class="card-body">
+					<div class="card-body shadow">
 						<p class="card-text text-right float-right"><i class="fas fa-fw fa-7x fa-skull text-muted"></i></p>
 						<h5 class="card-title">Death Report</h5>
 						<p class="card-text">Generate a death report.</p>
@@ -53,7 +53,7 @@
 		<div class="col-xl-6 mb-4">
 			<a href="/paperwork-generators/traffic-division-patrol-report">
 				<div class="card card-panel bg-dark text-white" id="card-generators-tdpatrol">
-					<div class="card-body">
+					<div class="card-body shadow">
 						<p class="card-text text-right float-right"><i class="fas fa-fw fa-7x fa-car-crash text-muted"></i></p>
 						<h5 class="card-title">Traffic Division: Patrol Report</h5>
 						<p class="card-text">Generate a traffic division patrol report.</p>
@@ -64,7 +64,7 @@
 		<div class="col-xl-6 mb-4">
 			<a href="/paperwork-generators/patrol-log">
 				<div class="card card-panel bg-dark text-white" id="card-generators-patrol">
-					<div class="card-body">
+					<div class="card-body shadow">
 						<p class="card-text text-right float-right"><i class="fas fa-fw fa-7x fa-clipboard-list text-muted"></i></p>
 						<h5 class="card-title">Patrol Log</h5>
 						<p class="card-text">Start your patrol log and generate a patrol log report at the end of shift.</p>
