ee0242f503606c585361a4fa37ad9148e5c37f4c
xanxTV <29733018+XanXTV@users.noreply.github.com>
2020-04-27 02:39:31 +0100

    Footer Transition Fix + New Text Colour

--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,5 @@
-<div id="footer" class="container text-center py-5" data-aos="fade-in" data-aos-duration="1000">
-	<img src="/images/Logo-MDC.png" width="80px"><i class="fas fa-fw fa-copyright ml-2" style="color: #e2b055;"></i> 2020
+<div class="container" data-aos="fade-in" data-aos-duration="1000">
+	<div id="footer" class="text-center py-5">
+		<img src="/images/Logo-MDC.png" width="80px"><i class="fas fa-fw fa-copyright ml-2 text-golden"></i> 2020
+	</div>
 </div>
