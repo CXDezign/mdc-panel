7733358d3b160e1260b43be40fe68b575d8e2f8b
xanxTV <29733018+XanXTV@users.noreply.github.com>
2020-05-26 21:21:47 +0100

    Unavailable Page Duplicate Code Removal

--- unavailable.php
+++ unavailable.php
@@ -1,63 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta property="og:title" content="MDC Panel">
-	<meta property="og:type" content="website">
-	<meta property="og:url" content="http://xanx.co.uk/MDC">
-	<meta property="og:image" content="http://xanx.co.uk/MDC/images/Logo-MDC.png">
-	<meta property="og:description" content="MDC Panel - Multi-functional tools, generators, and resources for official government use.">
-	<link rel="icon" type="image/png" href="favicon.png">
-
-	<title>MDC Panel</title>
-
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
-	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-	<!-- Styles -->
-	<link rel="stylesheet" type="text/css" href="/styles/custom.css?v=unavailable">
-
-	<!-- FontAwesome -->
-	<script src="https://kit.fontawesome.com/129680e694.js" crossorigin="anonymous"></script>
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-
-	<!-- Animate on Scroll -->
-	<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-
-	<!-- Bootstrap Switch Button -->
-	<link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css" rel="stylesheet">
-	<script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/js/bootstrap4-toggle.min.js"></script>
-
-	<!-- Map -->
-	<link rel="stylesheet" type="text/css" href="/map/style.css">
-
-	<!-- Leaflet -->
-	<link rel="stylesheet" type="text/css" href="https://unpkg.com/leaflet@1.1.0/dist/leaflet.css">
-	<script src="https://unpkg.com/leaflet@1.1.0/dist/leaflet.js"></script>
-
-	<!-- Leaflet Search -->
-	<!--<link rel="stylesheet" type="text/css" href="/map/src/leaflet-search.css">-->
-	<script src="/map/src/leaflet-search.js"></script>
-
-	<!-- Leaflet Font Awesome Icons -->
-	<link rel="stylesheet" type="text/css" href="/map/src/leaflet.awesome-markers.css">
-	<script src="/map/src/leaflet.awesome-markers.js"></script>
-</head>
-<body id="top">
-	<div class="wrapper">
-		<div class="container mt-5 p-5">
-			<div class="text-center">
-				<h1 style="font-size: 200px;"><i class="fas fa-fw fa-sad-tear"></i></h1>
-				<h3>
-				WEBSITE TEMPORARILY UNAVAILABLE</h3>
-				<p>The MDC Panel is currently under construction and will be unavailable until further notice.<br><br><br>
-				Questions? Message <strong>xanx#0001</strong> on Discord.</p>
-			</div>
+<div class="wrapper">
+	<div class="container mt-5 p-5">
+		<div class="text-center">
+			<h1 style="font-size: 200px;"><i class="fas fa-fw fa-sad-tear"></i></h1>
+			<h3>
+			WEBSITE TEMPORARILY UNAVAILABLE</h3>
+			<p>The MDC Panel is currently under construction and will be unavailable until further notice.<br><br><br>
+			Questions? Message <strong>xanx#0001</strong> on Discord.</p>
 		</div>
 	</div>
-</body>
-</html>
+</div>
