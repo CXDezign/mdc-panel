bd0867c2259080e4690762bcc726469e04953647
xanxTV <29733018+XanXTV@users.noreply.github.com>
2020-06-03 13:47:37 +0100

    Initialisation Scripts Moved to Footer

--- index.php
+++ index.php
@@ -70,16 +70,6 @@
 			</div>
 		</div>
 	</div>
-<script type="text/javascript">
-	AOS.init();
-
-	$(document).ready(function(){
-
-		// Select Picker Initialisation
-		$('.selectpicker').selectpicker();
-		
-	});
-</script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
--- templates/footer.php
+++ templates/footer.php
@@ -3,3 +3,13 @@
 		<img src="/images/Logo-MDC.png" alt="MDC Logo - Footer" width="80px"><i class="fas fa-fw fa-copyright ml-2 text-golden"></i><?= $g->getUNIX('year') ?>
 	</div>
 </div>
+<script type="text/javascript">
+	AOS.init();
+
+	$(document).ready(function(){
+
+		// Select Picker Initialisation
+		$('.selectpicker').selectpicker();
+		
+	});
+</script>
