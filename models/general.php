94a8d3ad36722a23c31a14fbed73164d384b0c60
xanxTV <29733018+XanXTV@users.noreply.github.com>
2020-06-25 04:02:39 +0100

    1.15.3 Version Control + Changelog

--- models/general.php
+++ models/general.php
@@ -12,7 +12,7 @@ class General {
 			case 'site-name':
 				return 'MDC Panel';
 			case 'site-version':
-				return '1.15.2';
+				return '1.15.3';
 			case 'site-url':
 				return $_SERVER['SERVER_NAME'];
 			case 'site-logo':
