c50e63f2cef71c62485155268807468eb88cc54d
xanxTV <29733018+XanXTV@users.noreply.github.com>
2020-07-06 21:54:59 +0100

    Illegal Parking Gen - Number shuffle

--- models/paperwork-generators.php
+++ models/paperwork-generators.php
@@ -535,8 +535,8 @@ class ParkingTicketGenerator extends PaperworkGenerators {
 
 		$reasonsVS = array(0);
 		$reasonsPR = array(1,2,3,4,5);
-		$reasonsOS = array(6,7,8,9,10,11,12,17);
-		$reasonsSW = array(13,14,15,16);
+		$reasonsOS = array(6,7,8,9,10,11,12,13);
+		$reasonsSW = array(14,15,16,17);
 
 		foreach ($illegalParkingReasons as $illegalParkingReason) {
 
