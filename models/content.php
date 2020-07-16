525ebb722593fff3a3716564871bdf967208f7d5
xanxTV <29733018+XanXTV@users.noreply.github.com>
2020-07-16 12:35:14 +0100

    Live Visitors Disable on Local + Project Fix Missing File

--- models/content.php
+++ models/content.php
@@ -1,5 +1,8 @@
 <?php
 
+$root = $_SERVER['DOCUMENT_ROOT'];
+require_once $root . '/models/general.php';
+
 class Content {
 
 	// FORMS
@@ -65,79 +68,87 @@ class Content {
 
 	public function visitors() {
 
-		$dbfile = "db/visitors.json";
-		$expire = 3600;
+		$g = new General();
 
-		if (!file_exists($dbfile)) {
-			throw new UnexpectedValueException("Error: Data file " . $dbfile . " NOT FOUND!");
-		}
+		if ($g->getSettings('site-live')) {
 
-		if (!is_writable($dbfile)) {
-			throw new UnexpectedValueException("Error: Data file " . $dbfile . " is NOT writable! Please CHMOD it to 666!");
-		}
+			$dbfile = "db/visitors.json";
+			$expire = 3600;
 
-		function in_array_r($needle, $haystack, $strict = false) {
-			foreach ($haystack as $item) {
-				if (($strict ? $item === $needle : $item == $needle) || (is_array($item) && in_array_r($needle, $item, $strict))) {
-					return true;
-				}
+			if (!file_exists($dbfile)) {
+				throw new UnexpectedValueException("Error: Data file " . $dbfile . " NOT FOUND!");
 			}
-			return false;
-		}
 
-		function CountVisitors($dbfile, $expire) {
+			if (!is_writable($dbfile)) {
+				throw new UnexpectedValueException("Error: Data file " . $dbfile . " is NOT writable! Please CHMOD it to 666!");
+			}
 
-			$currentIP = getIP();
-			$currentTime = time();
-
-			$visitors = json_decode(file_get_contents($dbfile), true);
-
-			if (!empty($visitors)) {
-
-				foreach ($visitors as $iVisitor => $visitor) {
-					$sameVisitorKey[] = implode(array_keys($visitor));
-					$sameVisitorVal[] = implode(array_values($visitor));
-				}
-
-				if (in_array_r($currentIP, $sameVisitorKey)) {
-					foreach($sameVisitorKey as $iVisitor => $visitor) {
-
-						if ($visitor == $currentIP) {
-							// If Current IP matches IP in array and stored expire time is in future, refresh expire time to current time
-							$visitors[$iVisitor][$visitor] = $currentTime;
-						} elseif (($visitor != $currentIP) && ($sameVisitorVal[$iVisitor]+$expire < $currentTime)) {
-							// If Current IP doesn't match IP in array and stored expire time is in the past, remove entry
-							unset($visitors[$iVisitor]);
-						}
+			function in_array_r($needle, $haystack, $strict = false) {
+				foreach ($haystack as $item) {
+					if (($strict ? $item === $needle : $item == $needle) || (is_array($item) && in_array_r($needle, $item, $strict))) {
+						return true;
 					}
-				} else {
-					// If array is filled with only 1 visitor and doesn't match any IPs
-					$newVisitor = array($currentIP => $currentTime);
-					array_push($visitors, $newVisitor);
 				}
-
-				// Reindex array
-				$visitors = array_values($visitors);
-				// JSON encode and return output
-				file_put_contents($dbfile, json_encode($visitors));
-				return count($visitors);
-			} else {
-				$visistorsNew = array();
-				$newVisitor = array($currentIP => $currentTime);
-				array_push($visistorsNew, $newVisitor);
-				file_put_contents($dbfile, json_encode($visistorsNew));
-				return count($visistorsNew);
+				return false;
 			}
+
+			function CountVisitors($dbfile, $expire) {
+
+				$currentIP = getIP();
+				$currentTime = time();
+
+				$visitors = json_decode(file_get_contents($dbfile), true);
+
+				if (!empty($visitors)) {
+
+					foreach ($visitors as $iVisitor => $visitor) {
+						$sameVisitorKey[] = implode(array_keys($visitor));
+						$sameVisitorVal[] = implode(array_values($visitor));
+					}
+
+					if (in_array_r($currentIP, $sameVisitorKey)) {
+						foreach($sameVisitorKey as $iVisitor => $visitor) {
+
+							if ($visitor == $currentIP) {
+								// If Current IP matches IP in array and stored expire time is in future, refresh expire time to current time
+								$visitors[$iVisitor][$visitor] = $currentTime;
+							} elseif (($visitor != $currentIP) && ($sameVisitorVal[$iVisitor]+$expire < $currentTime)) {
+								// If Current IP doesn't match IP in array and stored expire time is in the past, remove entry
+								unset($visitors[$iVisitor]);
+							}
+						}
+					} else {
+						// If array is filled with only 1 visitor and doesn't match any IPs
+						$newVisitor = array($currentIP => $currentTime);
+						array_push($visitors, $newVisitor);
+					}
+
+					// Reindex array
+					$visitors = array_values($visitors);
+					// JSON encode and return output
+					file_put_contents($dbfile, json_encode($visitors));
+					return count($visitors);
+				} else {
+					$visistorsNew = array();
+					$newVisitor = array($currentIP => $currentTime);
+					array_push($visistorsNew, $newVisitor);
+					file_put_contents($dbfile, json_encode($visistorsNew));
+					return count($visistorsNew);
+				}
+			}
+
+			function getIP() {
+
+				$ip = $_SERVER['REMOTE_ADDR'];
+				return ip2long($ip);
+
+			}
+		 
+			return CountVisitors($dbfile, $expire);
+
+		} else {
+			return 'LOCAL-SITE';
 		}
-
-		function getIP() {
-
-			$ip = $_SERVER['REMOTE_ADDR'];
-			return ip2long($ip);
-
-		}
-	 
-		return CountVisitors($dbfile, $expire);
 
 	}
 
